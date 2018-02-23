d3b261440319b816c56bb36e6c1e26de2fb3b513
Chris Poole <poolecs@gmail.com>
2018-02-23 13:45:16 -0800

    Update ops files/tooling

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: path.resolve(__dirname, "client", "dist"),
+    path: path.resolve(__dirname, "client", "public", "dist"),
     filename: "compiled.js"
   },
   module: {
