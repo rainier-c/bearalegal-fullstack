30202c06f01324dc916aa6a2e9940da39fe0ba6a
Chris Poole <poolecs@gmail.com>
2018-02-23 13:05:14 -0800

    Install React, Babel, Webpack

---
+++ client/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>Howdy world!</div>
+    );
+  }
+
+};
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+// webpack config
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, "client", "dist"),
+    filename: "compiled.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'] 
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+
+    ]
+  }
+};
